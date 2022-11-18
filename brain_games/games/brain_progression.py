93b016f736b1120f95bb64d55f62ad2d2b65d44b
Dmitry Shahov <demid58000@yandex.ru>
2022-11-18 16:32:43 +0300

    0,5

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -3,7 +3,7 @@ import random
 rule = "What number is missing in the progression?"
 
 def start_game():
-    first_number = random.randint(2, 10)
+    first_number = random.randint(0, 5)
     second_number = random.randint(15, 20)
     step = random.randint(1, 3)
     lst_ = []
