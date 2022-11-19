15e8a0aab863a5f6356fc225a921077bec5fa9f6
Dmitry Shahov <demid58000@yandex.ru>
2022-11-19 13:19:08 +0300

    chenged

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,7 +6,7 @@ def welcome_user(prolog=''):
     if prolog:
         print(f'{prolog}')
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
+    print(f'Hello, {name}!')
     return name
 
 
--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -14,7 +14,7 @@ def generate_answer_and_question():
     prime = ''
     question = randint(0, 100)
     if IsPrime(question) is True:
-        prime = 'no'
-    elif IsPrime(question) is False:
         prime = 'yes'
+    elif IsPrime(question) is False:
+        prime = 'no'
     return (question, prime)
