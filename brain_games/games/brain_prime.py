7aa4323a75230a813c99a2bd88d0ad5467db8dfa
Dmitry Shahov <demid58000@yandex.ru>
2022-11-19 13:12:49 +0300

    brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -14,7 +14,7 @@ def generate_answer_and_question():
     prime = ''
     question = randint(0, 100)
     if IsPrime(question) is True:
-        prime = 'yes'
-    elif IsPrime(question) is False:
         prime = 'no'
+    elif IsPrime(question) is False:
+        prime = 'yes'
     return (question, prime)
